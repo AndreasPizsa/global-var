acd0e0a185543b0fdcdc172c515bb3326daea341
Andreas Pizsa <andreas.pizsa@oebb.at>
2016-02-16 10:41:49 +0100

    test: require('./index)

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ require('should');
 describe('global-var', function() {
   describe('node', function() {
     it('returns `global`', function() {
-      const globals = require('.');
+      const globals = require('./index');
       globals.getGlobal().should.equal(global);
     });
   });
